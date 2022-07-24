4dfb5f52e1f860736fccc37e0907144a63428e68
Samuel Obembe <samuel@pop-os.localdomain>
2022-07-24 12:07:43 -0500

    moved things around, updated notes

---
+++ sampleProjects/3_ImplementAzureSecurity/microsoftGraphSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+
+namespace MicrosoftGraphSample{
+    public class Program{
+        public static void Main(string[] args){
+            Console.WriteLine("Howdi pop os");
+
+            IPublicClientApplication publicClientApplication = PublicClientApplicationBuilder.Create("").Build();
+
+            var tentantId = "common";
+            var clientId = "clientId";
+
+            var options = new TokenCredentialOptions{
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+            };
+
+            Func<DeviceCodeInfo,CancellationToken, Task> callback = (code, cancellation)=>{
+                Console.WriteLine(code.Message);
+    return Task.FromResult(0);
+            };
+
+            DeviceCodeCredential credential = new DeviceCodeCredential(callback, tentantId, clientId, options);
+
+            var graphClient = new GraphServiceClient(credential);            
+
+        }
+    }
+}
